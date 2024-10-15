ab97503e1dfa77c669b822d4374e15e6a28d4e62
Tim Lappalainen <timnhl95@hotmail.com>
2024-10-15 13:31:38 +0200

    Added comments and creator name

--- Program.cs
+++ Program.cs
@@ -1,16 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Tim Lapplainen #NET2024
 
 using System.IO.Pipes;
 using System.Reflection.Metadata;
 
-Stack<Employee> Employees = new Stack<Employee>();
+Stack<Employee> Employees = new Stack<Employee>();  //Creating a stack of employee
 
-Employee Cassandra = new Employee("001", "Cassandra", "Female", 10000);
+Employee Cassandra = new Employee("001", "Cassandra", "Female", 10000); //Creating new objects of employees
 Employee Kalle = new Employee("002", "Kalle", "Male", 11000);
 Employee Anton = new Employee("003", "Anton", "Male", 12000);
 Employee Sebbe = new Employee("004", "Sebbe", "Male", 13000);
 Employee Sara = new Employee("005", "Sara", "Female", 14000);
-Employees.Push(Cassandra);
+Employees.Push(Cassandra); //Adding the objects to the stack.
 Employees.Push(Kalle);
 Employees.Push(Anton);
 Employees.Push(Sebbe);
@@ -20,7 +20,7 @@ Employees.Push(Sara);
 
 
 Console.WriteLine("Personer i stacken");
-foreach(Employee person in Employees)
+foreach(Employee person in Employees)//Looping through all the persons in the stack and writing their name and id
 {
     person.PersonName();
     Console.WriteLine($"Antal personer i stacken: {Employees.Count}");
@@ -28,7 +28,7 @@ foreach(Employee person in Employees)
 }
 Console.WriteLine("\n Pop:");
 
-for(int i = Employees.Count; i > 0; i--)
+for(int i = Employees.Count; i > 0; i--) //Retrieving the employees through the use of Pop
 {
     var test = Employees.Pop();
     Console.WriteLine(test.Name);
@@ -42,14 +42,14 @@ Employees.Push(Anton);
 Employees.Push(Sebbe);
 Employees.Push(Sara);
 Console.WriteLine("\n Peek:");
-for (int i = 0; i < 2; i++)
+for (int i = 0; i < 2; i++) //Retrieving the user through the use of Peek
 {
     Console.WriteLine(Employees.Peek().Name);
 
     Console.WriteLine($"Antal personer i stacken: {Employees.Count}");
 }
 
-if(Employees.Contains(Anton))
+if(Employees.Contains(Anton)) //Checking if Anton exists in the stack
 {
     Console.WriteLine("Anton existerar i stacken");
 }
@@ -59,7 +59,7 @@ else
 }
 
 
-List<Employee> EmployeeList = new List<Employee>(
+List<Employee> EmployeeList = new List<Employee>( //Creating a list of employees
     new Employee[]
     {
         Cassandra,
@@ -69,7 +69,7 @@ List<Employee> EmployeeList = new List<Employee>(
         Sara
     });
 
-if(EmployeeList.Contains(Cassandra))
+if(EmployeeList.Contains(Cassandra)) //Checking if Cassandra exists in the list
 {
     Console.WriteLine("\nCassandra objektet existerar i listan");
 }
@@ -77,14 +77,14 @@ else
 {
     Console.WriteLine("Cassandra objektet existerar inte i listan");
 }
-Console.WriteLine("\nFörsta mannen i objektet");
+Console.WriteLine("\nFörsta mannen i objektet"); 
 var FirstMale = EmployeeList.Find(x => x.Gender == "Male");
 
-Console.WriteLine($"Namn: {FirstMale.Name} ID: {FirstMale.Id}");
-Console.WriteLine("\nAlla männen i objektet");
+Console.WriteLine($"Namn: {FirstMale.Name} ID: {FirstMale.Id}");//Printing the first male in the object
+Console.WriteLine("\nAlla männen i objektet"); 
 List<Employee> AllMales = EmployeeList.FindAll(x => x.Gender == "Male");
 
-foreach(Employee males in AllMales)
+foreach(Employee males in AllMales)//Printing all the males in the object
 {
     Console.WriteLine($"Name: {males.Name} ID: {males.Id}");
 }
