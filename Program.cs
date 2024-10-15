2f1ea729fc1919e59a84850aac21c21600f1e9b2
Tim Lappalainen <timnhl95@hotmail.com>
2024-10-15 11:18:49 +0200

    added peek

--- Program.cs
+++ Program.cs
@@ -5,16 +5,28 @@ using System.Reflection.Metadata;
 
 Stack<Employee> Employees = new Stack<Employee>();
 
-AddEmployees();
+Employee Olle = new Employee("001", "Olle", "Male", 10000);
+Employee Kalle = new Employee("002", "Kalle", "Male", 11000);
+Employee Anton = new Employee("003", "Anton", "Male", 12000);
+Employee Sebbe = new Employee("004", "Sebbe", "Male", 13000);
+Employee Robin = new Employee("005", "Robin", "Male", 14000);
+Employees.Push(Olle);
+Employees.Push(Kalle);
+Employees.Push(Anton);
+Employees.Push(Sebbe);
+Employees.Push(Robin);
 
 
 
+
+Console.WriteLine("Personer i stacken");
 foreach(Employee person in Employees)
 {
     person.PersonName();
     Console.WriteLine($"Antal personer i stacken: {Employees.Count}");
 
 }
+Console.WriteLine("\n Pop:");
 
 for(int i = Employees.Count; i > 0; i--)
 {
@@ -24,22 +36,24 @@ for(int i = Employees.Count; i > 0; i--)
 }
 
 
-
-
-
-void AddEmployees()
+Employees.Push(Olle);
+Employees.Push(Kalle);
+Employees.Push(Anton);
+Employees.Push(Sebbe);
+Employees.Push(Robin);
+Console.WriteLine("\n Peek:");
+for (int i = 0; i < 2; i++)
 {
-    Employee Olle = new Employee("001", "Olle", "Male", 10000);
-    Employee Kalle = new Employee("002", "Kalle", "Male", 11000);
-    Employee Anton = new Employee("003", "Anton", "Male", 12000);
-    Employee Sebbe = new Employee("004", "Sebbe", "Male", 13000);
-    Employee Robin = new Employee("005", "Robin", "Male", 14000);
-    Employees.Push(Olle);
-    Employees.Push(Kalle);
-    Employees.Push(Anton);
-    Employees.Push(Sebbe);
-    Employees.Push(Robin);
-}
+    Console.WriteLine(Employees.Peek().Name);
+
+    Console.WriteLine($"Antal personer i stacken: {Employees.Count}");
+}    
+
+
+
+
+
+
 
 
 public class Employee
